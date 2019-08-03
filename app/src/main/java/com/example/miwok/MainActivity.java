78d9688f33d05e6c59c48db2f928fe81f4f6b2b3
Pavel Ratushny <rpavelu@gmail.com>
2019-08-03 11:42:07 +0700

    Initial commit

---
+++ app/src/main/java/com/example/miwok/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.miwok;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void openNumbersList(View view) {
+        Intent i = new Intent(this, NumbersActivity.class);
+        startActivity(i);
+    }
+    public void openFamilyList(View view) {
+        Intent i = new Intent(this, FamilyActivity.class);
+        startActivity(i);
+    }
+    public void openColorsList(View view) {
+        Intent i = new Intent(this, ColorsActivity.class);
+        startActivity(i);
+    }
+    public void openPhrasesList(View view) {
+        Intent i = new Intent(this, PhrasesActivity.class);
+        startActivity(i);
+    }
+
+}
